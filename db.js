99c4952183f192f006e729b678ab1a7f1fb4847d
zkldi <ddhitone@gmail.com>
2020-10-25 07:47:55 +0000

    Test CI deploy

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const url = 'localhost:27017/kamaitachidb';
 const db = monk(url);
 
 console.log("Connecting to database...");
-console.time("DB CONNECT TIME")
+console.time("DB CONNECT TIME");
+
 db.then(() => {
     console.log("Connection to database successful.");
     console.timeEnd("DB CONNECT TIME");
